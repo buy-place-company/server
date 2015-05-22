4a851caaf8b4e6efd6a8ea5448eadbb35f159558
Ruslan Galimov <ruslan.galimov@corp.mail.ru>
2015-05-22 18:07:13 +0300

    well done

--- subsystems/foursquare/api.py
+++ subsystems/foursquare/api.py
@@ -40,7 +40,7 @@ class FoursquareAPI:
             lst_id = lst['list'].get('id', '')
 
         print(lst_id)
-        zone = ZoneMock.create(sw_lat=sw_lat, sw_lng=sw_lng, ne_lat=ne_lat, ne_lng=ne_lng, list_id=lst_id, is_activate=0)
+        zone = ZoneMock.get(sw_lat=sw_lat, sw_lng=sw_lng, ne_lat=ne_lat, ne_lng=ne_lng, list_id=lst_id)
 
         for venue in venues:
             if venue.get('id', ''):
