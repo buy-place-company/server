6b71b09429534c319813fdd86c45ae5cd1b2a60a
Ruslan Galimov <ruslan.galimov@corp.mail.ru>
2015-05-22 19:34:53 +0300

    well done

--- subsystems/foursquare/api.py
+++ subsystems/foursquare/api.py
@@ -30,25 +30,23 @@ class FoursquareAPI:
         venue.user_count = venue_raw['stats']['usersCount']
         venue.tip_count = venue_raw['stats']['tipCount']
         venue.name = re.sub(r'[^a-zа-яA-ZА-Я ]', "", venue_raw['name'])
+        # TODO: список категорий
         venue.category = venue_raw['categories'][0]['pluralName']
-        # print(venue_raw['categories'][0])
-        #venue.category = venue_raw['category']
         print("Added " + venue.name)
         return venue
 
     @staticmethod
-    def new_zone(sw_lat=None, sw_lng=None, ne_lat=None, ne_lng=None):
-        if sw_lat is None or sw_lng is None or ne_lat is None or ne_lng is None:
-            return None
-
+    def new_zone_list(zone):
         if FoursquareAPI.self is None:
             FoursquareAPI.self = FoursquareAPI()
 
-        venues = FoursquareAPI.self.client.venues.search(params={"intent": "browse", "sw": "%F,%F" % (sw_lat, sw_lng),
-                                              "ne": "%F,%F" % (ne_lat, ne_lng),
-                                              "categotyId": DEFAULT_CATEGORIES})['venues']
+        venues = FoursquareAPI.self.client.venues.search(
+            params={"intent": "browse", "sw": "%F,%F" % (zone.sw_lat, zone.sw_lng),
+                    "ne": "%F,%F" % (zone.ne_lat, zone.ne_lng),
+                    "categoryId": DEFAULT_CATEGORIES})['venues']
         try:
-            lst = FoursquareAPI.self.client.lists.add({'name': 'sw{0:.3}_{1:.3}_ne{2:.3}_{3:.3}'.format(sw_lat, sw_lng, ne_lat, ne_lng)})
+            lst = FoursquareAPI.self.client.lists.add(
+                {'name': 'sw{0:.3}_{1:.3}_ne{2:.3}_{3:.3}'.format(zone.sw_lat, zone.sw_lng, zone.ne_lat, zone.ne_lng)})
         except FoursquareException:
             print("Zone with such corrds exists already")
             return None
@@ -59,8 +57,6 @@ class FoursquareAPI:
         else:
             lst_id = lst['list'].get('id', '')
 
-        print(lst_id)
-        zone = ZoneMock()  # .get(sw_lat=sw_lat, sw_lng=sw_lng, ne_lat=ne_lat, ne_lng=ne_lng)
         zone.list_id = lst_id
         zone.save()
 
@@ -74,18 +70,10 @@ class FoursquareAPI:
 
     @staticmethod
     def get_venue(id):
-        # try:
-        #     venue = Venue.objects.get(venue_id=id)
-        # except Venue.DoesNotExist:
-        #     venue = None
-        #
-        # if venue:
-        #     return venue
-        # else:
-            if not FoursquareAPI.self:
-                FoursquareAPI.self = FoursquareAPI()
-            item = FoursquareAPI.self.client.venues(venue_id=id)
-            venue = FoursquareAPI.venue_from_item(item, item['venue']['id'])
-            venue.save()
-            return venue
+        if not FoursquareAPI.self:
+            FoursquareAPI.self = FoursquareAPI()
+        item = FoursquareAPI.self.client.venues(venue_id=id)
+        venue = FoursquareAPI.venue_from_item(item, item['venue']['id'])
+        venue.save()
+        return venue
 
