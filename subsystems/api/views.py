17ba81225b679b26a046a5a1555b408bbbdc1e25
ruslan.galimov <ruslan.galimov@corp.mail.ru>
2015-07-13 14:09:02 +0300

    Merge

--- subsystems/api/views.py
+++ subsystems/api/views.py
@@ -1,10 +1,8 @@
 import json
-import datetime
 import urllib.request
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
-from conf import secret
 from subsystems.api.errors import GameError, NoMoneyError, HasOwnerAlready, UHaveIt, UDontHaveIt
 from subsystems.api.utils import JSONResponse, VenueView
 from subsystems.db.model_user import User
@@ -13,111 +11,10 @@ from subsystems.db.model_zone import Zone
 from subsystems.foursquare.api import FoursquareAPI
 from conf.settings_local import SettingsLocal
 from conf.secret import VK_APP_KEY, VK_APP_ID
-from conf.settings_game import ORDER_BY, DEFAULT_CATEGORIES
-from subsystems.foursquare.utils.foursquare_api import Foursquare, ServerError
+from conf.settings_game import ORDER_BY
 
 redirect_url = "http://yandex.ru"
 # TODO: security!!!
-
-
-class ZoneView:
-    def __init__(self, sw_lat, sw_lng, ne_lat, ne_lng, list_id=None):
-        self.list_id = list_id
-        self.ne_lat = ne_lat
-        self.ne_lng = ne_lng
-        self.sw_lng = sw_lng
-        self.sw_lat = sw_lat
-
-        self.client = Foursquare(client_id=secret.client_id, client_secret=secret.secret_id, redirect_uri=redirect_url)
-        self._venues = None
-
-    def create(self, name=None):
-        if name is None:
-            name = "auto_" + self.sw_lat + self.sw_lng + self.ne_lat + self.ne_lng
-        resp = self.client.lists.add({'name': name})
-        self.list_id = resp.get('id')
-
-    def venues(self, force_update=False):
-        if self._venues:
-            return self._venues
-
-        if not self.list_id:
-            self.create()
-
-        if not force_update:
-            venues = list(Venue.objects.filter(list_id=self.list_id).values()[:50])
-
-        if force_update or not venues:
-            try:
-                venues = self.client.venue.search(sw="{0},{1}".format(self.sw_lat, self.sw_lng),
-                                                  ne="{0},{1}".format(self.ne_lat, self.ne_lng),
-                                                  categoryId=DEFAULT_CATEGORIES)
-            except ServerError:
-                return None
-            for ven in venues:
-                ven['list_id'] = self.list_id
-        self.save_venues()
-
-        venues = list(Venue.objects.filter(list_id=self.list_id).values()[:50])
-        self._venues = venues
-        return venues
-
-    def save_venues(self):
-        obj = Zone.objects.get('list_id', None)
-        obj.timestamp = datetime.datetime.now()
-        obj.save()
-        Venue.objects.bulk_create(self.venues)
-
-
-class VenueView:
-    def __init__(self, venue_id):
-        self.venue = Venue.objects.get(venue_id=venue_id)
-
-    def __getitem__(self, item):
-        return self.venue.get(item)
-
-    def buy(self, user):
-        if user.cash < self.venue.price:
-            raise NoMoneyError
-
-        if self.venue.owner == user:
-            raise UHaveIt
-
-        if self.venue.owner:
-            raise HasOwnerAlready
-
-        self.venue.owner = user
-        user.cash -= self.venue.price
-        user.score += self.venue.price
-        user.buildings_count += 1
-        user.save()
-        self.venue.save()
-
-    def sell(self, user):
-        if self.venue.owner != user:
-            raise UDontHaveIt
-
-        self.venue.owner = None
-        user.cash += self.venue.price
-        user.score -= self.venue.price
-        user.buildings_count -= 1
-        user.save()
-        self.venue.save()
-
-    def upgrade(self, user):
-        price = self.venue.price * (1 + self.venue.lvl ** 1.1) / (1 + self.venue.lvl) * (1 - DUTY)
-
-        if self.venue.owner != user:
-            raise UDontHaveIt
-
-        if user.cash < price:
-            raise NoMoneyError
-
-        user.cash -= price
-        user.score += price
-        user.save()
-        self.venue.save()
-
 
 @csrf_exempt
 def zone_venues(request):
