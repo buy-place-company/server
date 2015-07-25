13c504c013e61fa1e55a99d36399aa21a25f4a20
Galimov Ruslan <delfin1995@yandex.ru>
2015-07-25 17:01:10 +0300

    Fix

--- subsystems/db/model_venue.py
+++ subsystems/db/model_venue.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 from datetime import timedelta, datetime
+import django.dispatch
 
 from django.db import models
 
 from .model_user import User
-# from subsystems.api.signals import venue_push
 import hashlib
 
 BASE_COST = 300
 BASE_INCOME = 100
 TIME_DELTA = 30
+
+venue_push = django.dispatch.Signal(providing_args=["fields"])
 
 class VenueManager(models.Manager):
     def get_queryset(self):
@@ -36,7 +38,6 @@ class Venue(models.Model):
     owner = models.ForeignKey(User, null=True, blank=True)
     lat = models.FloatField(default=0)
     lng = models.FloatField(default=0)
-    push_check_sum = models.CharField(max_length=33)
 
     # private information
     loot = models.IntegerField(default=0)
@@ -129,7 +130,5 @@ class Venue(models.Model):
     def save(self, *args, **kwargs):
         self.last_update = datetime.now().timestamp()
         m = hashlib.md5()
-        m.update("000005fab4534d05api_key9a0554259914a86fb9e7eb014e4e5d52permswrite")
-        print(m.hexdigest())
-        # venue_push.send(sender=self.__class__, fields=[])
+        m.update("000005fab4534d05api_key9a0554259914a86fb9e7eb014e4e5d52permswrite".encode('utf-8'))
         super(Venue, self).save(*args, **kwargs)
