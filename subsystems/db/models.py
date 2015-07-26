aa7eb76ab6c4cf4691e47dcb851b44c7b2010981
ksan <kgfq@mail.ru>
2015-07-26 18:18:07 +0300

    avatar

--- subsystems/db/models.py
+++ subsystems/db/models.py
@@ -3,3 +3,23 @@ from .model_venue import Venue
 from .model_zone import Zone
 from .model_bookmark import Bookmark
 from .model_deal import Deal
+
+
+class Venue:
+    pass
+
+
+class User:
+    pass
+
+
+class Zone:
+    pass
+
+
+class Deal:
+    pass
+
+
+class Bookmark:
+    pass
