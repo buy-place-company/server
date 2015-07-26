737ddc5988e80b38b9d4546e68a806481522c609
ksan <kgfq@mail.ru>
2015-07-26 18:26:22 +0300

    avatar

--- subsystems/db/model_venue.py
+++ subsystems/db/model_venue.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from datetime import timedelta, datetime
+import hashlib
+from django.db.models import ForeignKey
 
 import django.dispatch
 from django.db import models
@@ -143,3 +145,23 @@ class Venue(models.Model):
     def save(self, *args, **kwargs):
         self.last_update = datetime.now().timestamp()
         super(Venue, self).save(*args, **kwargs)
+
+
+class Bookmark(mo.Model):
+    # system fields
+    user = ForeignKey(mo.User)
+    venue = ForeignKey(Venue)
+    push_check_sum = models.CharField(max_length=33)
+    is_autocreated = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        m = hashlib.md5()
+        m.update(self.content_object.check_sum.encode('utf-8'))
+        self.push_check_sum = m.hexdigest()
+        super(Bookmark, self).save(*args, **kwargs)
+
+    def serialize(self, user_owner=None, **kwargs):
+        return {
+            'user': self.user.serialize(user_owner=user_owner),
+            str(self.content_type): self.content_object.serialize(user_owner=user_owner, **kwargs),
+        }
--- subsystems/db/models.py
+++ subsystems/db/models.py
@@ -2,26 +2,3 @@ from .model_user import User
 from .model_venue import Venue
 from .model_zone import Zone
 from .model_deal import Deal
-import hashlib
-from django.db import models as mo
-from django.db.models import ForeignKey
-
-
-class Bookmark(mo.Model):
-    # system fields
-    user = ForeignKey(User)
-    venue = ForeignKey(Venue)
-    push_check_sum = mo.CharField(max_length=33)
-    is_autocreated = mo.BooleanField(default=True)
-
-    def save(self, *args, **kwargs):
-        m = hashlib.md5()
-        m.update(self.content_object.check_sum.encode('utf-8'))
-        self.push_check_sum = m.hexdigest()
-        super(Bookmark, self).save(*args, **kwargs)
-
-    def serialize(self, user_owner=None, **kwargs):
-        return {
-            'user': self.user.serialize(user_owner=user_owner),
-            str(self.content_type): self.content_object.serialize(user_owner=user_owner, **kwargs),
-        }
