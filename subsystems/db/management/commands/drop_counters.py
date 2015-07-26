889e3e67323c1e15f8b513d4a48a7e531342734f
ksan <kgfq@mail.ru>
2015-07-26 20:46:11 +0300

    logout auth_required

--- subsystems/db/management/commands/drop_counters.py
+++ subsystems/db/management/commands/drop_counters.py
@@ -8,4 +8,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         for user in User.objects.all():
-            user.building_counter = len(Venue.objects.filter(owner=self))
+            user.building_counter = len(Venue.objects.filter(owner=user))
